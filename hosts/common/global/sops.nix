44e78e462dcbe98925cdab3d7bcde2af197613db
Jhonatan Rosário <contato@devjhon.com>
2026-04-05 16:17:20 -0300

    refactor: remove pacotes do sops da configuração do sistema e atualiza dependências do shell de desenvolvimento

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -3,11 +3,6 @@ let
   userPath = config.users.users.jhonatan.home;
 in
 {
-  environment.systemPackages = with pkgs; [
-    age
-    sops
-  ];
-
   sops = {
     defaultSopsFile = ../../../secrets/secrets.yaml;
     age.keyFile = "${userPath}/.config/sops/age/keys.txt";
