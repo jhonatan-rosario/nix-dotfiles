23cba26ea7a3365a05c9f86e4201c1c2594f0625
Jhonatan Rosário <contato@devjhon.com>
2026-04-08 14:05:56 -0300

    fix: adiciona a opção de montagem umask=0077 à partição de boot na configuração de hardware

--- hosts/voyager/hardware-configuration.nix
+++ hosts/voyager/hardware-configuration.nix
@@ -43,6 +43,7 @@
             type = "filesystem";
             format = "vfat";
             mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
           };
         };
         luks = {
