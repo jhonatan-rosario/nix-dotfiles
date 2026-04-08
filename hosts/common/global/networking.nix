4b5701f9a3295d682fbb69343c6865eb977b0f04
Jhonatan Rosário <contato@devjhon.com>
2026-04-08 02:30:08 -0300

    refactor: atualiza perfis de rede

--- hosts/common/global/networking.nix
+++ hosts/common/global/networking.nix
@@ -9,83 +9,131 @@
     ensureProfiles = {
       environmentFiles = [ config.sops.secrets.wireless-env.path ];
       profiles = {
-        home-wifi = {
+        CLARO_2DFE61_5G = {
           connection = {
-            id = "home-wifi";
-            permissions = "";
+            id = "CLARO_2DFE61_5G";
+            interface-name = "wlp2s0";
             type = "wifi";
-            autoconnect = true;
+            uuid = "7ab65817-7bc8-4a3e-9ebb-4c8ea2977548";
           };
-
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
+          proxy = { };
           wifi = {
-            ssid = "CLARO_2DFE61";
             mode = "infrastructure";
-          };
-
-          wifi-security = {
-            key-mgmt = "wpa-psk";
-            psk = "$HOME_WIFI_PASSWORD";
-          };
-        };
-
-        home-wifi-5g = {
-          connection = {
-            id = "home-wifi-5g";
-            permissions = "";
-            type = "wifi";
-            autoconnect = true;
-          };
-
-          wifi = {
             ssid = "CLARO_2DFE61_5G";
-            mode = "infrastructure";
           };
-
           wifi-security = {
+            auth-alg = "open";
             key-mgmt = "wpa-psk";
             psk = "$HOME_WIFI_PASSWORD";
           };
         };
-
-        work-wifi = {
+        CLARO_2DFE61 = {
           connection = {
-            id = "work-wifi";
-            permissions = "";
+            id = "CLARO_2DFE61";
+            interface-name = "wlp2s0";
             type = "wifi";
-            autoconnect = true;
+            uuid = "7ab65817-7bc8-4a3e-9ebb-4c8ea2977548";
           };
-
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
+          proxy = { };
           wifi = {
-            ssid = "UNIVALE - TI";
             mode = "infrastructure";
+            ssid = "CLARO_2DFE61";
           };
-
           wifi-security = {
+            auth-alg = "open";
+            key-mgmt = "wpa-psk";
+            psk = "HOME_WIFI_PASSWORD";
+          };
+        };
+        "UNIVALE - TI" = {
+          connection = {
+            id = "UNIVALE - TI";
+            interface-name = "wlp2s0";
+            type = "wifi";
+            uuid = "cf09a480-7621-4605-a0a1-2fa89d12cf8e";
+          };
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
+          proxy = { };
+          wifi = {
+            mode = "infrastructure";
+            ssid = "UNIVALE - TI";
+          };
+          wifi-security = {
+            auth-alg = "open";
             key-mgmt = "wpa-psk";
             psk = "$WORK_WIFI_PASSWORD";
           };
         };
-
-        work-wifi-5g = {
+        "UNIVALE - TI - 5G" = {
           connection = {
-            id = "work-wifi-5g";
-            permissions = "";
+            id = "UNIVALE - TI - 5G";
+            interface-name = "wlp2s0";
             type = "wifi";
-            autoconnect = true;
+            uuid = "94aa6f85-7fe0-4101-82c7-18b18a2ec03e";
           };
-
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
+          proxy = { };
           wifi = {
-            ssid = "UNIVALE - TI - 5G";
             mode = "infrastructure";
+            ssid = "UNIVALE - TI - 5G";
           };
-
           wifi-security = {
+            auth-alg = "open";
             key-mgmt = "wpa-psk";
             psk = "$WORK_WIFI_PASSWORD";
+          };
+        };
+        "Z Fold7 de Jhonatan" = {
+          connection = {
+            id = "Z Fold7 de Jhonatan";
+            interface-name = "wlp2s0";
+            type = "wifi";
+            uuid = "f3b66191-2f3c-494d-b7b1-310485101e03";
+          };
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
+          proxy = { };
+          wifi = {
+            mode = "infrastructure";
+            ssid = "Z Fold7 de Jhonatan";
+          };
+          wifi-security = {
+            key-mgmt = "sae";
+            psk = "MY_PHONE_WIFI_PASSWORD";
           };
         };
       };
     };
   };
-
 }
