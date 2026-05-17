d0ab6900a563e4882302268189bb02fe5e6d34b4
Jhonatan Rosário <contato@devjhon.com>
2026-05-17 15:12:55 -0300

    feat: adiciona novo driver ao cups para impressoras epson

--- hosts/common/optional/printing.nix
+++ hosts/common/optional/printing.nix
@@ -1,9 +1,30 @@
-{...}: {
+{ pkgs, ... }:
+{
   # Enable CUPS to print documents.
-  services.printing.enable = true;
   services.avahi = {
     enable = true;
     nssmdns4 = true;
     openFirewall = true;
   };
+
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      # cups-browsed
+      epson-escpr2
+    ];
+  };
+
+  # hardware.printers = {
+  #   ensureDefaultPrinter = "Epson_L3250_TI";
+  #   ensurePrinters = [
+  #     {
+  #       name = "Epson_L3250_TI";
+  #       description = "Epson L3250";
+  #       location = "TI";
+  #       deviceUri = "ipp://192.168.50.122:631/ipp/print";
+  #       model = "everywhere";
+  #     }
+  #   ];
+  # };
 }
