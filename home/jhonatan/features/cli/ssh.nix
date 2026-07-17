c7fd1616a1ae7580dda488d58e3c37d4c827d56a
Jhonatan Rosário <contato@devjhon.com>
2026-07-17 13:22:15 -0300

    fix: corrige permissões da configuração ssh via script de ativação

--- home/jhonatan/features/cli/ssh.nix
+++ home/jhonatan/features/cli/ssh.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{
+  lib,
+  ...
+}:
 {
   programs.ssh = {
     enable = true;
@@ -10,6 +13,23 @@
         IdentityFile = "~/.ssh/id_ed25519";
       };
     };
+  };
+
+  home.file = {
+    # home-manager wrongly thinks it doesn't manage (and thus shouldn't clobber) this file due to the activation script
+    ".ssh/config".force = true;
+  };
+
+  home.activation = {
+    # https://github.com/nix-community/home-manager/issues/322
+    fixSshPermissions = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+      run install -d -m 0700 "$HOME/.ssh"
+      if [ -L "$HOME/.ssh/config" ]; then
+        src="$(readlink -f "$HOME/.ssh/config")"
+        run rm -f "$HOME/.ssh/config"
+        run install -m 0600 "$src" "$HOME/.ssh/config"
+      fi
+    '';
   };
 
   home.persistence."/persist" = {
