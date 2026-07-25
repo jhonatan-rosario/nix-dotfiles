e61e9fe4f05251169c3a71200c417935f94a0a32
Jhonatan Rosário <contato@devjhon.com>
2026-07-25 19:33:45 -0300

    refactor(mobile): renomeia módulo android para mobile-dev e adiciona google-chrome
    - Renomeia `android.nix` para `mobile.nix` e altera o nome do ambiente FHS para `mobile-dev`
    - Adiciona `google-chrome` e exporta `CHROME_EXECUTABLE` para suporte a Flutter Web
    - Adiciona customização do prompt do shell Fish dentro do ambiente FHS

--- home/jhonatan/features/programming/mobile.nix
+++ home/jhonatan/features/programming/mobile.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 let
-  android-fhs = (
+  mobile-fhs = (
     pkgs.buildFHSEnv {
-      name = "android-dev";
+      name = "mobile-dev";
       targetPkgs =
         pkgs: with pkgs; [
           # Bibliotecas que os binários do Google (adb, aapt2, etc) exigem
@@ -44,20 +44,35 @@ let
           ninja
           which
 
+          google-chrome
           # Adicione outras libs que o Expo ou Flutter reclamar
         ];
       runScript = "fish"; # Ou seu shell de preferência
       profile = ''
         export ANDROID_HOME="$HOME/Android/Sdk"
         export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
-        export PATH="$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/cmdline-tools/latest/bin"
+        export CHROME_EXECUTABLE=${pkgs.google-chrome}/bin/google-chrome
+        export PATH="$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/cmdline-tools/latest/bin:${pkgs.google-chrome}/bin"
+
+        # Customize o prompt do Fish
+        exec ${pkgs.fish}/bin/fish -C '
+            functions -q fish_prompt; and functions -c fish_prompt _old_fish_prompt
+            function fish_prompt
+              printf "\n%s%s%s\n%s[mobile]❯%s " \
+                (set_color --bold $fish_color_operator) \
+                (prompt_pwd) \
+                (set_color --reset) \
+                (set_color $fish_color_cwd) \
+                (set_color --reset)
+            end
+          '
       '';
     }
   );
 in
 {
   home.packages = [
-    android-fhs
+    mobile-fhs
   ];
 
   home.persistence."/persist" = {
