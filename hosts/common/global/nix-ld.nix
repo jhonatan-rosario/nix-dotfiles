3fd4f1c7f5fe4959881d12349116414720c32f1a
Jhonatan Rosário <contato@devjhon.com>
2026-08-04 03:01:17 -0300

    feat(nix-ld): adiciona bibliotecas para execucao do winbox

--- hosts/common/global/nix-ld.nix
+++ hosts/common/global/nix-ld.nix
@@ -6,5 +6,21 @@
 
   programs.nix-ld = {
     enable = true;
+    libraries = with pkgs; [
+      # Para executar o winbox
+      libxkbcommon
+      libxcb
+      libxcb-wm
+      libxcb-image
+      libxcb-keysyms
+      libxcb-render-util
+      libx11
+      libglvnd
+      freetype
+      fontconfig
+      zlib
+      glibc
+      dbus
+    ];
   };
 }
